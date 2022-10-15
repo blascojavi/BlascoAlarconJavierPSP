9626695a60e4b34c7d3ca1a9a226d3ac1e9d4b96
Javier Blasco <blascojavi@gmail.com>
2022-10-15 17:18:01 +0200

    significatiu

--- src/ud1/practices/IPAdressProcess.java
+++ src/ud1/practices/IPAdressProcess.java
@@ -1,4 +1,5 @@
 package ud1.practices;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,30 +8,56 @@ import java.io.InputStreamReader;
 public class IPAdressProcess {
     public static void main(String[] args) {
 
-            ProcessBuilder processBuilder = new ProcessBuilder();
+        //He deixat l'opció de realisar-lo per CMD o per WSL
+        cmd();//Por CMD ho deixe, ya que em va costar trobar-ho, pero després em vaig donar conte que ténia que fer-lo per WSL
+        wsl();
+    }
 
-            processBuilder.command("cmd.exe", "/c", "ipconfig");
+    private static void cmd() {
+        System.out.println("\n A través de CMD");
+        ProcessBuilder processBuilder = new ProcessBuilder();
 
-            try {
+        processBuilder.command("cmd.exe", "/c", "ipconfig");
 
-                Process process = processBuilder.start();
+        try {
 
-                BufferedReader reader
-                        = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            Process process = processBuilder.start();
 
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
-                }
+            BufferedReader reader
+                    = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
-                int exitCode = process.waitFor();
-                System.out.println("\nExited with error code : " + exitCode);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
             }
+
+            int exitCode = process.waitFor();
+            System.out.println("\nExited with error code : " + exitCode);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void wsl() {
+        System.out.println("\n A través de wsl");
+
+        ProcessBuilder process = new ProcessBuilder("wsl.exe", "ip", "-br", "a");
+        try {
+            Process pr = process.start();
+            BufferedReader stdout = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+            String line;
+            while ((line = stdout.readLine()) != null) {
+                if (line.startsWith("eth0")) {
+                    System.out.println("La direcció IP del dispositiu és : " + line.substring(32, 45));
+                }  }
+        }  catch (IOException e) {
+            e.printStackTrace();
         }
     }
+}
 
---
+++ src/ud1/practices/TextEditorProcess.java
@@ -0,0 +1,45 @@
+package ud1.practices;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TextEditorProcess {
+    public static void main(String[] args) {
+
+        String text =
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n" +
+                "Phasellus nec porttitor sem.\n" +
+                "Etiam sit amet risus luctus diam semper finibus ut nec mauris.\n" +
+                "Etiam a pulvinar tellus, non scelerisque ante.\n" +
+                "Sed egestas quam mollis nibh ornare semper mollis vel ex.\n" +
+                "Etiam congue finibus dui, ut mattis nibh vulputate et.\n" +
+                "Donec congue mauris ut nulla condimentum pellentesque.\n" +
+                "Cras sollicitudin congue porta. Nunc eu scelerisque sem. ";
+
+
+        ProcessBuilder processEscritura = new ProcessBuilder("wsl.exe", "echo", text, ">", "files/text.txt");
+        ProcessBuilder processLlectura = new ProcessBuilder("wsl.exe", "cat", "files/text.txt");
+
+
+        try {
+            processEscritura.start();
+            Process procesoLectura = processLlectura.start();
+
+
+            BufferedReader stdout = new BufferedReader(new InputStreamReader(procesoLectura.getInputStream()));
+            System.out.println("L'edició de text ha acabat.");
+            System.out.println("Contingut del fitxer \"text.txt\":");
+            String line;
+            while ((line = stdout.readLine()) != null)
+                System.out.printf("\t%s\n", line);
+
+        } catch (IOException ex) {
+            System.err.println("Excepció d'E/S.");
+            System.out.println(ex.getMessage());
+            System.exit(-1);
+        }
+
+
+    }
+}
