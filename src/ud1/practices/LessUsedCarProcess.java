2043a3bb2b7485e6af4126315190535bd8868a96
Javier Blasco <blascojavi@gmail.com>
2022-10-15 18:02:01 +0200

    significatiu

--- src/ud1/practices/LessUsedCarProcess.java
+++ src/ud1/practices/LessUsedCarProcess.java
@@ -8,11 +8,11 @@ import java.util.List;
 
 public class LessUsedCarProcess {
     public static void main(String[] args) {
-        //Creamos un aarrayList
+        //Creamos un arrayList
         ArrayList<ProcessBuilder> programs = new ArrayList<>();
         //llenamos el array con el archivo del concessionari
         programs.add(new ProcessBuilder("wsl.exe", "cat", "files/concessionari.csv"));
-        //Ordenamos el array desde la 3ªcolumnam cib separador de "," y por orden numerico (no alfabetico)
+        //Ordenamos el array desde la 3ªcolumna con separador de "," y por orden numerico (no alfabetico)
         programs.add(new ProcessBuilder("wsl.exe", "sort", "-k3", "-t,", "-n"));
         //Mostramos solo el primer elemento
         programs.add(new ProcessBuilder("wsl.exe", "head", "-1"));
@@ -20,21 +20,20 @@ public class LessUsedCarProcess {
         programs.add(new ProcessBuilder("wsl.exe", "cut", "-d,", "-f2,3"));
 
         try {
+            //inicia un proceso para PB y crea un pipeline que esta vinculado por los flujos de E/S estandar
             List<Process> proc = ProcessBuilder.startPipeline(programs);
-
-
+            //cogémos el último proceso (sería el proceso 1 pero le restamos 1 para cogerlo correctamente)
             Process last = proc.get(proc.size() - 1);
+            //Sacamos el resultado del proceso
             BufferedReader stdout = new BufferedReader(new InputStreamReader(last.getInputStream()));
-
-            System.out.println(stdout.readLine());
-
+            //imprimimos el resultado
+            System.out.println("El coche que tiene menos kilometros es: \n" + stdout.readLine());
 
         } catch (IOException e) {
-            System.err.println("Excepció d'E/S.");
+            System.err.println("Excepció d'E / S.");
             System.out.println(e.getMessage());
             System.exit(-1);
         }
-
 
     }
 }
