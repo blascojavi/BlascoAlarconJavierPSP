1791e14f174f4ff7be3ad0f8dd16b57d645fd520
Javier Blasco <blascojavi@gmail.com>
2022-10-24 17:22:52 +0200

    significatiu

--- src/ud1/practices/IPAdressProcess.java
+++ src/ud1/practices/IPAdressProcess.java
@@ -8,55 +8,62 @@ public class IPAdressProcess {
     public static void main(String[] args) {
 
         //He deixat l'opció de realisar-lo per CMD o per WSL
-        cmd();//Por CMD ho deixe, ya que em va costar trobar-ho, pero després em vaig donar conte que ténia que fer-lo per WSL
+        //cmd();//Por CMD ho deixe, ya que em va costar trobar-ho, pero després em vaig donar conte que ténia que fer-lo per WSL
         wsl();
     }
-
-    private static void cmd() {
-        System.out.println("\n A través de CMD");
-        ProcessBuilder processBuilder = new ProcessBuilder();
-
-        processBuilder.command("cmd.exe", "/c", "ipconfig");
-
-        try {
-
-            Process process = processBuilder.start();
-
-            BufferedReader reader
-                    = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-
-            int exitCode = process.waitFor();
-            System.out.println("\nExited with error code : " + exitCode);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
     private static void wsl() {
         System.out.println("\n A través de wsl");
 
         ProcessBuilder process = new ProcessBuilder("wsl.exe", "ip", "-br", "a");
         try {
             Process pr = process.start();
+            pr.waitFor();
             BufferedReader stdout = new BufferedReader(new InputStreamReader(pr.getInputStream()));
             String line;
+
             while ((line = stdout.readLine()) != null) {
                 if (line.startsWith("eth0")) {
                     System.out.println("La direcció IP del dispositiu és : " + line.substring(32, 45));
                 }  }
         }  catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+
     }
+
+
+   // private static void cmd() {
+       // System.out.println("\n A través de CMD");
+       // ProcessBuilder processBuilder = new ProcessBuilder();
+
+        //processBuilder.command("cmd.exe", "/c", "ipconfig");
+
+       // try {
+
+         //   Process process = processBuilder.start();
+
+           // BufferedReader reader
+             //       = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+         //   String line;
+         //   while ((line = reader.readLine()) != null) {
+          //      System.out.println(line);
+          //  }
+
+         //   int exitCode = process.waitFor();
+          //  System.out.println("\nExited with error code : " + exitCode);
+
+      //  } catch (IOException e) {
+        //    e.printStackTrace();
+      //  } catch (InterruptedException e) {
+       //     e.printStackTrace();
+     //   }
+
+
+    //}
+
+
 }
 
